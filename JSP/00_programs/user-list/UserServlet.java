7be8ef91d61b357b43263662e05b52264bc580f0
taehvvan <th00514@naver.com>
2025-05-31 12:28:16 +0900

    Update UserServlet.java

--- JSP/00_programs/user-list/UserServlet.java
+++ JSP/00_programs/user-list/UserServlet.java
@@ -1,1 +1,20 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.util.*;
+
+public class UserServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        try {
+            UserDAO dao = new UserDAO();
+            List<User> list = dao.getAllUsers();
+            request.setAttribute("userList", list);
+            RequestDispatcher dispatcher = request.getRequestDispatcher("userList.jsp");
+            dispatcher.forward(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
 
