04efacc6b1ac66db21fefc954310f91d82bdeff1
taehvvan <th00514@naver.com>
2025-05-31 12:20:34 +0900

    Update User.java

--- JSP/00_programs/user-list/User.java
+++ JSP/00_programs/user-list/User.java
@@ -1,1 +1,40 @@
+public class User {
+    private int id;
+    private String name;
+    private String email;
 
+    // 기본 생성자
+    public User() {}
+
+    // 매개변수 있는 생성자 (선택사항)
+    public User(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    // Getter & Setter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
