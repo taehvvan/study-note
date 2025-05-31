68f3747030ea353ebd12d4e9687d37026929ab05
taehvvan <th00514@naver.com>
2025-05-31 12:14:24 +0900

    Update LoginServlet.java

--- JSP/06_mvc/LoginServlet.java
+++ JSP/06_mvc/LoginServlet.java
@@ -1,1 +1,20 @@
+import java.io.IOException;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String userId = request.getParameter("userId");
+        String password = request.getParameter("password");
+
+        if ("admin".equals(userId) && "1234".equals(password)) {
+            request.setAttribute("userId", userId);
+            RequestDispatcher dispatcher = request.getRequestDispatcher("success.jsp");
+            dispatcher.forward(request, response);
+        } else {
+            response.sendRedirect("login.jsp");
+        }
+    }
+}
 
