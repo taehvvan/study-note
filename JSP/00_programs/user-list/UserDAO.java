ee1cd31519a79db771013cf018db0fc36a3d04af
taehvvan <th00514@naver.com>
2025-05-31 12:20:55 +0900

    Update UserDAO.java

--- JSP/00_programs/user-list/UserDAO.java
+++ JSP/00_programs/user-list/UserDAO.java
@@ -1,1 +1,28 @@
+import java.sql.*;
+import java.util.*;
+
+public class UserDAO {
+    private Connection getConnection() throws Exception {
+        String url = "jdbc:mysql://localhost:3306/mydb";
+        String user = "root";
+        String pw = "1234";
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection(url, user, pw);
+    }
+
+    public List<User> getAllUsers() throws Exception {
+        List<User> list = new ArrayList<>();
+        String sql = "SELECT * FROM users";
+        try (Connection conn = getConnection(); PreparedStatement ps = conn.prepareStatement(sql); ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                User u = new User();
+                u.setId(rs.getInt("id"));
+                u.setName(rs.getString("name"));
+                u.setEmail(rs.getString("email"));
+                list.add(u);
+            }
+        }
+        return list;
+    }
+}
 
